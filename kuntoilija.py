f142ee62cf1b3818c4ae79386a46f0bdf409a5bb
janlevea <janlevea@gmail.com>
2023-02-17 14:28:38 +0200

    Lisää: USA_rasvaprosentin laskentametodi

--- kuntoilija.py
+++ kuntoilija.py
@@ -16,7 +16,7 @@ class Kuntoilija:
     """Luokka kuntoilijan tietoja varten"""
 
     # Olionmuodostin eli konstruktori, self -> tuleva olio
-    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli, vyotaron_ymparys=0.0, kaulan_ymparys=0.0, lantion_ymparys=0.0):
 
         # Määritellään tulevan olion ominaisuudet (property) eli luokan kentät (field)
         self.nimi = nimi
@@ -26,11 +26,33 @@ class Kuntoilija:
         self.sukupuoli = sukupuoli
         self.bmi = fitness.laske_bmi(self.paino, self.pituus)
 
+        self.vyotaron_ymparys = vyotaron_ymparys
+        self.kaulan_ymparys = kaulan_ymparys
+        self.lantion_ymparys = lantion_ymparys
+
     # Metodi rasvaprosentin laskemiseen (yleinen / aikuinen)
     def rasvaprosentti(self):
         self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(
             self.bmi, self.ika, self.sukupuoli)
         return self.rasvaprosentti
+
+    def usa_rasvaprosentti(self):
+        if self.sukupuoli == 1:
+            if self.vyotaron_ymparys == 0.0 or self.kaulan_ymparys == 0.0:
+                print("USA:n armeijan rasvaprosenttikaava vaatii vyötärön ja kaulan ympärysmitat.")
+                self.usa_rasvaprosentti = 0.0
+            else:
+                self.usa_rasvaprosentti = fitness.usa_rasvaprosentti_mies(
+                    self.pituus, self.vyotaron_ymparys, self.kaulan_ymparys
+                )
+        elif self.sukupuoli == 0:
+            if self.vyotaron_ymparys == 0.0 or self.kaulan_ymparys == 0.0 or self.lantion_ymparys == 0.0:
+                print("USA:n armeijan rasvaprosenttikaava vaatii vyötärön, kaulan ja lantion ympärysmitat.")
+                self.usa_rasvaprosentti = 0.0
+            else:
+                self.usa_rasvaprosentti = fitness.usa_rasvaprosentti_nainen(
+                    self.pituus, self.vyotaron_ymparys, self.kaulan_ymparys, self.lantion_ymparys
+                )
 
 # JunioriKuntoilija-luokka Kuntoilija-luokan aliluokka (subclass)
 
